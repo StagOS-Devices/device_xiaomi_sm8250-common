744be562a143ff471e45e4649447a6a62576000e
drkphnx <dark.phnx12@gmail.com>
2021-11-27 21:51:06 +0530

    drop remove packages

--- kona.mk
+++ kona.mk
@@ -372,10 +372,6 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
     
-# RemovePackages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
