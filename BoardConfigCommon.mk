8fc8dbad0f80d2e0c43e42ec40ef6260abc3b8d1
Stallix <joey@evolution-x.org>
2021-12-03 19:37:09 +0530

    Add Additional flag for kernel

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/alioth
+TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Partitions
