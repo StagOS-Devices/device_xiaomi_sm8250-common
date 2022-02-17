c5a10fc6134fa040739d01f03c65197e12a0bc8d
johnmart19 <johnivan19999@gmail.com>
2022-02-17 13:54:10 +0200

    sm8250-common: Declare compilation for android.hardware.power@1.2
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- kona.mk
+++ kona.mk
@@ -461,6 +461,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2 \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
